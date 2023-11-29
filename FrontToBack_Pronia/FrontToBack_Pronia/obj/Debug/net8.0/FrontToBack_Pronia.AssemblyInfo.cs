0be15259a90fd163f70aa6fc0a80eb6a10fad868
Ayshen Salahova <TU81UEBFJ@code.edu.az>
2023-11-29 10:46:47 +0400

    accept added

--- FrontToBack_Pronia/FrontToBack_Pronia/obj/Debug/net8.0/FrontToBack_Pronia.AssemblyInfo.cs
+++ FrontToBack_Pronia/FrontToBack_Pronia/obj/Debug/net8.0/FrontToBack_Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontToBack_Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cfdb1e1b34e69f2b7cf49b3139adc9fe8ba36d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0ca4ed9191d621386d12ea77a8602af3502c35")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontToBack_Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontToBack_Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
